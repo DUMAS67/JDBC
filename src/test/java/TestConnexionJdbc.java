d39511acad0275d8649d18fd46f7092d98e64ce6
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-12-10 16:46:02 +0100

    TP2 version améliorée avec driver en fichier log

--- src/test/java/TestConnexionJdbc.java
+++ src/test/java/TestConnexionJdbc.java
@@ -5,23 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-import org.mariadb.jdbc.Driver;
-
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			DriverManager.registerDriver(new Driver());
+			
 			ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
 			String user = monFichierConf.getString("user");
 			String password = monFichierConf.getString("password");
 			String url = "jdbc:" + monFichierConf.getString("url");
+			String driver =monFichierConf.getString("driver");
+			Class.forName(driver);
 			Connection maConnection = DriverManager.getConnection(url, user, password);
 			System.out.println(maConnection.getCatalog());
 			maConnection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Le Driver driver n'a pas été trouvé");
 		}
 	}
 }
