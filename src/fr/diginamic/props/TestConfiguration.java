3e49d4d7a07a938c527028cdbcaa03732fbcd75e
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-12-10 14:13:03 +0100

    TP 1 "connexion fichier.properties"

--- src/fr/diginamic/props/TestConfiguration.java
+++ src/fr/diginamic/props/TestConfiguration.java
@@ -7,7 +7,6 @@ import java.util.Set;
 public class TestConfiguration {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		ResourceBundle monFichierConf = ResourceBundle.getBundle("monfichier");
 		String driverName = monFichierConf.getString("user");
